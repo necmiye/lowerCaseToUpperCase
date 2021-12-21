0f8f4cbdd1a0b0d627d9400b929a4835a2df5ce4
necmiye <nejmiye.arabaci@tein.com.tr>
2021-12-22 00:45:37 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [textInput, setTextInput] = useState("");
+    const [textShow, setTextShow] = useState("");
+    function handleClick () {
+
+        setTextShow(textInput.toUpperCase());
+        console.log(textInput)
+    }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div container style={{textAlign:"center"}}>
+              <div>
+                  <label style={{maxWidth:"100%",maxHeight:"100%", marginRight:"10px"}}>
+                      enter some character :
+                  </label>
+              </div>
+              <div>
+                      <input
+                          type={"text"}
+                          aria-label={"enter some character"}
+                          color={"black"}
+                          value={textInput}
+                          onChange={(e)=>setTextInput(e.target.value)}
+                          style={{width:"250px",height:"40px", marginRight:"10px"}}
+                      />
+                      <button
+                          style={{width:"70px",height:"40px", marginRight:"10px"}}
+                          onClick={handleClick}
+                      >
+                          SHOW
+                      </button>
+              </div>
+              <div>
+                  <label className='capatilize'>
+                      {textShow}
+                  </label>
+              </div>
+          </div>
       </header>
     </div>
   );
